3fb60986a958c1bda7310752498a532fd492a27d
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-15 17:46:05 -0500

    Change from representational component to class component.

--- todolist-react/src/components/ItemsList.js
+++ todolist-react/src/components/ItemsList.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { API_ROOT, HEADERS } from '../utils/constants';
 
-const ItemsList = props => {
-  return (
-    <ul>
-      {props.items.map((item, index) => {
-        return (
-          <li key={index}>{item.description}</li>
-        );
-      })}
-    </ul>
-  );
+class ItemsList extends Component {
+  onCompleteHandler = (itemId) => (e) => {
+    e.preventDefault();
+
+    console.info(itemId)
+
+    fetch(`${API_ROOT}/items/${itemId}`, {
+      method: 'PUT',
+      headers: HEADERS,
+      body: JSON.stringify({is_done: true})
+    });
+
+    //this.setState({ itemInput: '' });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item, index) => {
+          return (
+            <li
+              onClick={this.onCompleteHandler(item.id)}
+              key={index}
+            >
+              {item.description}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 };
 
 export default ItemsList;
