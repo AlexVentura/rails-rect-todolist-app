7165cd96bdd92e7d1ee1fcfd6f017a1bc0018c2a
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-15 14:02:18 -0500

    Implement the idex action to return all items and set strong params.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
+    items = Item.all
+    render json: items
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:description, :is_done)
   end
 end
