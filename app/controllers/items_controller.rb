7f88b457f6a299aae4afd024d9e734f5e756de51
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-15 17:45:08 -0500

    Add update action to complete items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:update]
+
   def index
     items = Item.pending
     render json: items
@@ -6,6 +8,7 @@ class ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
+
     if item.save
       serialized_data = ActiveModelSerializers::Adapter::Json.new(
         ItemSerializer.new(item)
@@ -16,7 +19,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /items/1
+  def update
+    if @item.update(item_params)
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        ItemSerializer.new(Item.pending)
+      ).serializable_hash
+
+      ActionCable.server.broadcast 'items_channel', serialized_data
+      head :ok
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:description, :is_done)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index, :create]
+  resources :items, only: [:index, :create, :update]
   mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
