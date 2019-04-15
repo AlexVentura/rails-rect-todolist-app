3b508d1d53d20374280bb15d3cdc2b6c6ba949d7
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-15 16:33:22 -0500

    Add create action to items controller, serialize object and stream it through action cable server.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,18 @@ class ItemsController < ApplicationController
     render json: items
   end
 
+  def create
+    item = Item.new(item_params)
+    if item.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        ItemSerializer.new(item)
+      ).serializable_hash
+
+      ActionCable.server.broadcast 'items_channel', serialized_data
+      head :ok
+    end
+  end
+
   private
 
   def item_params
