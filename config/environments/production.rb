a79d84cce6b0ebf7d49b1e84632617309465463e
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-16 10:42:44 -0500

    Configure action cable for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://todolist-app-cable.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://todolist-app-cable.herokuapp.com/', 'http://todolist-app-cable.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
