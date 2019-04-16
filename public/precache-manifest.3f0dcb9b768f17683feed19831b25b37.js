2283f1e8436694e7868257417bee98453ea36a66
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2019-04-16 10:42:26 -0500

    Create a new controller to render the react app.

---
+++ public/precache-manifest.3f0dcb9b768f17683feed19831b25b37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "365290bac6b4cda61709",
+    "url": "/static/js/main.cc4076e3.chunk.js"
+  },
+  {
+    "revision": "8962cc246c91735c3717",
+    "url": "/static/js/2.efd5e7e3.chunk.js"
+  },
+  {
+    "revision": "fd9d465dd644d166f0baf6ea8af284d4",
+    "url": "/index.html"
+  }
+];
